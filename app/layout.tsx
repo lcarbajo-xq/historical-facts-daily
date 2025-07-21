a4a78fb688139799c694d6c3f69eb04ccafc66c9
lcarbajo-xq <luis.carbajo.arguelles@gmail.com>
2025-07-21 12:03:42 +0200

    chore: añade archivos layout y page relacionados con el footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Footer } from '@/components/ui/footer'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Historical Facts Daily',
+  description:
+    'Descubre hechos históricos interesantes que ocurrieron en cada día del año',
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='es'>
+      <body>
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
